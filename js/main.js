e4a0703d959e1eae307a238f936abbea70b69b95
DouglasWilliam <dwillsouza@gmail.com>
2021-01-26 20:28:11 -0300

    Array e Dicionário

--- js/main.js
+++ js/main.js
@@ -1,13 +1,39 @@
 
 
-var nome = "Douglas William";
-var n1 = 5;
-var n2 = 3; 
-var frase = "Japão é o melhor time do mundo"
+var frutas = [{nome: "maça" , cor: "vermelha"} , {nome: "uva" , cor: "roxa"}]
+console.log(frutas);
+alert(frutas[1].nome);
+
+
+/*
+var fruta = {nome: "maça" , cor: "vermelha"}
+console.log(fruta);
+console.log(fruta.nome);
+alert(fruta.cor);
+*/
+
+/*
+var lista = ["maça" , "pêra" , "laranja"];
+console.log(lista);
+//lista.pop();
+//lista.push("uva")
+*/
+
+/*
+console.log(lista[0]);
+console.log(lista.toString());
+console.log(lista.join(" - "));
+*/
+
+
+//var nome = "Douglas William";
+//var n1 = 5;
+//var n2 = 3; 
+//var frase = "Japão é o melhor time do mundo"
 //alert(nome + " tem " + idade + " anos ");
 //alert(idade + idade2);
- console.log(nome);
- console.log(n1 * n2);
- console.log(frase.replace("Japão" , "Brasil"));
-console.log(frase.toLowerCase());
+ //console.log(nome);
+ //console.log(n1 * n2);
+ //console.log(frase.replace("Japão" , "Brasil"));
+//console.log(frase.toLowerCase());
  //alert(frase.replace("Japão" , "Brasil"));
