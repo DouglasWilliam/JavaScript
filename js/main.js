afef7ad36f928f799fc54305e34d1d774939b4e5
DouglasWilliam <dwillsouza@gmail.com>
2021-01-26 19:12:58 -0300

    Introdução ao JavaScript

---
+++ js/main.js
@@ -0,0 +1,13 @@
+
+
+var nome = "Douglas William";
+var n1 = 5;
+var n2 = 3; 
+var frase = "Japão é o melhor time do mundo"
+//alert(nome + " tem " + idade + " anos ");
+//alert(idade + idade2);
+ console.log(nome);
+ console.log(n1 * n2);
+ console.log(frase.replace("Japão" , "Brasil"));
+console.log(frase.toLowerCase());
+ //alert(frase.replace("Japão" , "Brasil"));
